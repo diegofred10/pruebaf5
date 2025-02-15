b9f5205789f296528fa641dcb821b809346434af
diegofred10 <diegoa.gsm@gmail.com>
2025-02-15 23:30:43 +0100

    first commit frontend

---
+++ src/main/java/com/example/Pruebaf5/Pruebaf5Application.java
@@ -0,0 +1,13 @@
+package com.example.Pruebaf5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pruebaf5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pruebaf5Application.class, args);
+	}
+
+}
